d945e6293db076645205834b50e70c2457410db6
andys <andysmith836@gmail.com>
2023-10-03 22:07:38 +0100

    Rover returns it's origin.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn execute(_commands: &str) -> String {
+    "0:0:N".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_stay_at_origin() {
+        let result = execute("");
+        assert_eq!(result, "0:0:N")
+    }
 }
