52c13bb0db6e2734ad79c290c5e6948ff324fdb7
andys <andysmith836@gmail.com>
2023-10-04 19:38:12 +0100

    Extract out compass struct

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,8 @@
+mod compass;
+
+use crate::compass::Compass;
+
 fn main() {}
-
-
-pub struct Compass {
-    direction_index: usize,
-    directions: [String; 4],
-}
-
-impl Compass {
-    pub fn new() -> Compass {
-        Compass{direction_index: 0, directions: ["N".to_string(), "E".to_string(), "S".to_string(), "W".to_string()]}
-    }
-
-    pub fn left(&mut self) {
-        if self.direction_index > 0 {
-            self.direction_index -= 1;
-        } else {
-            self.direction_index = 3;
-        }
-    }
-
-    pub fn right(&mut self) {
-        self.direction_index += 1;
-        if self.direction_index > 3 {
-            self.direction_index = 0;
-        }
-    }
-
-    fn get_direction(&mut self) -> String {
-        self.directions[self.direction_index].to_string()
-    }
-}
-
 
 fn execute(commands: &str) -> String {
     let mut y = 0;
