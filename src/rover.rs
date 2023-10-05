c9d0ec85e586a9dd4644f582b9e029d705ff38b4
andys <andysmith836@gmail.com>
2023-10-05 20:50:40 +0100

    Extract out a Coordinte struct

--- src/rover.rs
+++ src/rover.rs
@@ -1,14 +1,18 @@
 use crate::compass::Compass;
 
-pub struct Rover {
+struct Coordinate {
     x: i32,
-    y: i32,
+    y: i32
+}
+
+pub struct Rover {
+    coordinate: Coordinate,
     compass: Compass
 }
 
 impl Rover {
     pub fn new() -> Rover {
-        Rover{x: 0, y: 0, compass: Compass::new()}
+        Rover{coordinate:Coordinate { x: 0, y: 0 }, compass: Compass::new()}
     }
 
     pub fn turn_left(&mut self) {
@@ -21,31 +25,31 @@ impl Rover {
 
     pub fn forward(&mut self) {
         if self.compass.get_direction() == "E" {
-            self.x += 1;
-            if self.x > 9 {
-                self.x = 0;
+            self.coordinate.x += 1;
+            if self.coordinate.x > 9 {
+                self.coordinate.x = 0;
             }
         } else if self.compass.get_direction() == "S" {
-            if self.y > 0 {
-                self.y -= 1;
+            if self.coordinate.y > 0 {
+                self.coordinate.y -= 1;
             } else {
-                self.y = 9;
+                self.coordinate.y = 9;
             }
         } else if self.compass.get_direction() == "W" {
-            if self.x > 0 {
-                self.x -= 1;
+            if self.coordinate.x > 0 {
+                self.coordinate.x -= 1;
             } else {
-                self.x = 9;
+                self.coordinate.x = 9;
             }
         } else {
-            self.y += 1;
-            if self.y > 9 {
-                self.y = 0;
+            self.coordinate.y += 1;
+            if self.coordinate.y > 9 {
+                self.coordinate.y = 0;
             }
         }
     }
 
     pub fn location(&mut self) -> String {
-        format!("{}:{}:{}", self.x, self.y, self.compass.get_direction())
+        format!("{}:{}:{}", self.coordinate.x, self.coordinate.y, self.compass.get_direction())
     }
 }
