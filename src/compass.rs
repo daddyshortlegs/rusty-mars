2f06849d02158dc7d0910bff74798079befcc3ce
andys <andysmith836@gmail.com>
2023-10-05 21:48:29 +0100

    Simplify name

--- src/compass.rs
+++ src/compass.rs
@@ -1,29 +1,29 @@
 pub struct Compass {
-    direction_index: usize,
-    directions: [String; 4],
+    directions: [char; 4],
+    index: usize,
 }
 
 impl Compass {
     pub fn new() -> Compass {
-        Compass{direction_index: 0, directions: ["N".to_string(), "E".to_string(), "S".to_string(), "W".to_string()]}
+        Compass{index: 0, directions: ['N', 'E', 'S', 'W']}
     }
 
     pub fn left(&mut self) {
-        if self.direction_index > 0 {
-            self.direction_index -= 1;
+        if self.index > 0 {
+            self.index -= 1;
         } else {
-            self.direction_index = 3;
+            self.index = 3;
         }
     }
 
     pub fn right(&mut self) {
-        self.direction_index += 1;
-        if self.direction_index > 3 {
-            self.direction_index = 0;
+        self.index += 1;
+        if self.index > 3 {
+            self.index = 0;
         }
     }
 
     pub fn get_direction(&mut self) -> String {
-        self.directions[self.direction_index].to_string()
+        self.directions[self.index].to_string()
     }
 }
